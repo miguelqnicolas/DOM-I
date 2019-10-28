434bd7e040592c640011a8d8cc334452b4dcd23c
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-10-28 19:04:37 -0400

    wip

--- js/index.js
+++ js/index.js
@@ -6,10 +6,11 @@ const siteContent = {
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
+    "img-src": "img/logo.png",
+    "addition": "I was added!"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM<br> Is<br> Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -28,7 +29,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address" : "123 Way 456 Street<br>Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -37,6 +38,89 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
+// example: update the img src for the logo
+
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+// nav bar
+
+const navLink1 = document.querySelector("nav a:nth-child(1)");
+navLink1.innerHTML = siteContent["nav"]["nav-item-1"];
+navLink1.style.color = "green";
+
+const navLink2 = document.querySelector("nav a:nth-child(2)");
+navLink2.innerHTML = siteContent["nav"]["nav-item-2"];
+navLink2.style.color = "green";
+
+const navLink3 = document.querySelector("nav a:nth-child(3)");
+navLink3.innerHTML = siteContent["nav"]["nav-item-3"];
+navLink3.style.color = "green";
+
+const navLink4 = document.querySelector("nav a:nth-child(4)");
+navLink4.innerHTML = siteContent["nav"]["nav-item-4"];
+navLink4.style.color = "green";
+
+const navLink5 = document.querySelector("nav a:nth-child(5)");
+navLink5.innerHTML = siteContent["nav"]["nav-item-5"];
+navLink5.style.color = "green";
+
+const navLink6 = document.querySelector("nav a:nth-child(6)");
+navLink6.innerHTML = siteContent["nav"]["nav-item-6"];
+navLink6.style.color = "green";
+
+const appendChild = document.querySelector("nav a");
+appendChild.appendChild(siteContent["nav"]["addition"]);
+
+// first section
+
+// difference between innerhtml and textcontent?
+
+//centered cta
+
+const cta = document.querySelector(".cta");
+cta.display = 'flex';
+cta.style.justifyContent = 'center';
+
+// howcome first one works but other does not
+
+const firstSectionText = document.querySelector(".cta-text h1");
+firstSectionText.innerHTML = siteContent["cta"]["h1"];
+
+const firstSectionButton = document.querySelector(".cta-text button");
+firstSectionButton.innerHTML = siteContent["cta"]["button"];
+
+const firstSectionImage = document.querySelector("#cta-img");
+firstSectionImage.setAttribute("src", siteContent["cta"]["img-src"]);
+
+// second section
+
+const secondSectionImage = document.querySelector("#middle-img");
+secondSectionImage.src = siteContent["main-content"]["middle-img-src"];
+
+const secondSectionSubtitles = document.querySelectorAll(".text-content h4");
+secondSectionSubtitles[0].textContent = siteContent["main-content"]["features-h4"];
+secondSectionSubtitles[1].textContent = siteContent["main-content"]["about-h4"];
+secondSectionSubtitles[2].textContent = siteContent["main-content"]["services-h4"];
+secondSectionSubtitles[3].textContent = siteContent["main-content"]["product-h4"];
+secondSectionSubtitles[4].textContent = siteContent["main-content"]["vision-h4"];
+
+const secondSectionParagraphs = document.querySelectorAll(".text-content p");
+secondSectionParagraphs[0].textContent = siteContent["main-content"]["features-content"];
+secondSectionParagraphs[1].textContent = siteContent["main-content"]["about-content"];
+secondSectionParagraphs[2].textContent = siteContent["main-content"]["services-content"];
+secondSectionParagraphs[3].textContent = siteContent["main-content"]["product-content"];
+secondSectionParagraphs[4].textContent = siteContent["main-content"]["vision-content"];
+
+// footer
+
+const footerSubtitle = document.querySelector(".contact h4");
+footerSubtitle.textContent = siteContent["contact"]["contact-h4"];
+
+const footerParagraphs = document.querySelectorAll(".contact p");
+footerParagraphs[0].innerHTML = siteContent["contact"]["address"];
+footerParagraphs[1].textContent = siteContent["contact"]["phone"];
+footerParagraphs[2].textContent = siteContent["contact"]["email"];
+
+const footerCopyright = document.querySelector("footer p");
+footerCopyright.textContent = siteContent["footer"]["copyright"];
